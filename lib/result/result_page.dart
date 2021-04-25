861f29dfa7aab609124d8b8a0cf2294269ddae7b
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-04-25 14:51:50 -0300

    Final da aula 05

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -3,6 +3,7 @@ import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
 import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ResultPage extends StatelessWidget {
   final String title;
@@ -54,7 +55,9 @@ class ResultPage extends StatelessWidget {
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 68),
-                      child: NextButtonWidget.purple(label: "Compartilhar", onTap: () {}),
+                      child: NextButtonWidget.purple(label: "Compartilhar", onTap: () {
+                        Share.share('DevQuiz NLW#5 - Flutter: Resultadao do Quiz: $title\nObtive: ${result/length}% de aproveitamento!');
+                      }),
                   )),
                 ],
               ),
